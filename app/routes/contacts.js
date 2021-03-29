21f22d5254f8eed10ce86e03b905e9b1f35fe9b3
Patrick Kimber <code@pkimber.net>
2021-03-29 17:40:37 +0100

    'ember-django-adapter' get list of contacts

--- app/routes/contacts.js
+++ app/routes/contacts.js
@@ -1,9 +1,12 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class ContactsRoute extends Route {
+  @service store;
 
   model() {
-    return ['Marie Curie', 'Mae Jemison', 'Albert Hofmann'];
+    return this.store.findAll('contact');
+    // return ['Marie Curie', 'Mae Jemison', 'Albert Hofmann'];
   }
 }
 
