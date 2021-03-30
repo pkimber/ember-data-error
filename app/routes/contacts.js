69dc312139d886c95e88141ba65cc37b2fc33745
Patrick Kimber <code@pkimber.net>
2021-03-30 12:14:29 +0100

    'ember-concurrency' for model contacts

--- app/routes/contacts.js
+++ app/routes/contacts.js
@@ -2,12 +2,16 @@ import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency';
 
 export default class ContactsRoute extends Route {
   @service store;
 
   model() {
-    return this.store.findAll('contact')
+    return {
+      contacts: this.doMoreStuff.perform()
+    };
+    // return this.store.findAll('contact')
     /**
     return RSVP.hash({
       // return {
@@ -15,17 +19,15 @@ export default class ContactsRoute extends Route {
       contacts: this.store.findAll('contact'),
     })
     */
-
-
-
-    // try {
-    //   return this.store.findAll('contact');
-    // } catch (e) {
-    //   console.log("Before.................................................")
-    //   console.log(e)
-    //   console.log("After..................................................")
-    // }
   }
+
+  // try {
+  //   return this.store.findAll('contact');
+  // } catch (e) {
+  //   console.log("Before.................................................")
+  //   console.log(e)
+  //   console.log("After..................................................")
+  // }
 
   @action
   error(error, transition) {
@@ -42,16 +44,16 @@ export default class ContactsRoute extends Route {
     // }
   }
 
-  // @task *doMoreStuff() {
-  //   try {
-  //     let contact = yield this.store.findRecord("contact", 22, { reload: true })
-  //     this.director = contact
-  //   } catch(e) {
-  //     console.log("catch, doStuff .........................................")
-  //     // console.log(e)
-  //   } finally {
-  //     console.log("finally, doStuff .......................................")
-  //   }
-  // }
+  @task *doMoreStuff() {
+    try {
+      let contacts = yield this.store.findAll("contact", { reload: true })
+      return contacts
+    } catch(e) {
+      console.log("catch, doMoreStuff .....................................")
+      // console.log(e)
+    } finally {
+      console.log("finally, doMoreStuff ...................................")
+    }
+  }
 }
 
